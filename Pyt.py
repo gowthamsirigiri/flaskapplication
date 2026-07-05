591b7cd80dd3af65c4f1390a7d3e16f7f03bd44e
Gowtham Sirigiri <93024646+gowthamsirigiri@users.noreply.github.com>
2026-07-05 23:39:48 +0530

    Update Pyt.py

--- Pyt.py
+++ Pyt.py
@@ -18,4 +18,4 @@ def contact():
     return 'You can Contact to Gowtham Who is the Owner of this page! 😎'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
